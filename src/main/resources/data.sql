7b7b9e3370dd77bb026a4327a36be7144f3bf466
WardVanSteenwinkel <r0856666@student.thomasmore.be>
2022-01-13 00:06:15 +0100

    styling af (niet 100% responsive)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,109 +1,109 @@
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-    1, 'Kroontje', 'zwart', 'zoute caramel', '/img/pralines/271276791_508157460502944_7127541849248175643_n.jpg'
+    1, 'Kroontje', 'zwart', 'Dit kroontje is de ideale mix tussen intense zwarte chocolade en zoute caramel.', '/img/pralines/271276791_508157460502944_7127541849248175643_n.jpg'
 );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             2, 'Appelsien', 'wit', 'botercreme, appelsienconfituur', '/img/pralines/271512681_660544671765545_8066957524553581099_n.jpg'
+             2, 'Appelsien', 'wit', 'Deze opvallende praline bevat ambachtelijke sinaasappelconfituur, en zachte botercreme.', '/img/pralines/271512681_660544671765545_8066957524553581099_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             3, 'Manon', 'wit', 'koffiesmaak', '/img/pralines/271246139_683325369323589_2631719329763747776_n.jpg'
+             3, 'Manon', 'wit', 'Deze klassieker bevat zachte botercreme van koffie op een fijn laagje marsepein afgewerkt met een hele okkernoot.', '/img/pralines/271246139_683325369323589_2631719329763747776_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             4, 'Farao', 'melk', 'praline, nougatine, caramel', '/img/pralines/271483798_312759560865425_7836681827812214449_n.jpg'
+             4, 'Farao', 'melk', 'Praline, caramel en nougatine omringd door heerlijke melkchocolade.', '/img/pralines/271483798_312759560865425_7836681827812214449_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             5, 'blaadje', 'zwart', 'pistachemarsepein met rum', '/img/pralines/271257214_256375349797735_6472052609466035286_n.jpg'
+             5, 'Blaadje', 'zwart', 'Pistachemarsepein met rum omringd door intense zwarte chocolade.', '/img/pralines/271257214_256375349797735_6472052609466035286_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             6, 'aardbei', 'melk', 'botercreme, aardbeiconfituur', '/img/pralines/271408883_476932437343657_3158691092677106472_n.jpg'
+             6, 'Aardbei Melk', 'melk', 'Onder deze praline schuilt een heerlijke combinatie tussen ambachtelijke aardbeiconfituur en zachte botercreme.', '/img/pralines/271408883_476932437343657_3158691092677106472_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             7, 'ganache', 'melk', 'zwarte ganache', '/img/pralines/271383545_447786283705312_6943723555794085727_n.jpg'
+             7, 'Ganache', 'melk', 'Zelfgemaakte melkchocolade met ganache van pure chocolade.', '/img/pralines/271383545_447786283705312_6943723555794085727_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             8, 'Witte bol', 'wit', 'botercreme, advocaat', '/img/pralines/271440657_3077824709200500_4383051319098221704_n.jpg'
+             8, 'Witte bol', 'wit', 'Een smeuïge vulling van botercrème en advocaat.', '/img/pralines/271440657_3077824709200500_4383051319098221704_n.jpg'
          );
 
 insert into PRALINE (
     ID, PRALINE_NAME, CHOCOLATE_TYPE, INFO, IMG_SOURCE
 ) VALUES (
-             9, 'Manon F', 'zwart', 'praline, caramel, okkernoot', '/img/pralines/271234155_319699276749957_4000328248914636269_n.jpg'
+             9, 'Manon Fondant', 'zwart', 'Zachte praline, gezouten caramel en afgewerkt met een hele okkernoot.', '/img/pralines/271234155_319699276749957_4000328248914636269_n.jpg'
          );
 
 insert into Vulling (
                      ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-         1, 'Praline', false , 'mengsel van hazelnootpasta en chocolade'
+         1, 'Praline', false , 'Mengsel van hazelnootpasta en chocolade.'
         );
 
     insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    2, 'Botercreme', true , 'mengsel van boter en suikerwater'
+    2, 'Botercreme', true , 'Mengsel van boter en suikerwater.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    3, 'Marsepein', false , 'mengsel van amandelen en suiker'
+    3, 'Marsepein', false , 'Mengsel van amandelen en bloemsuiker.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    4, 'Ganache', true , 'mengsel van chocolade, room en boter'
+    4, 'Ganache', true , 'Mengsel van chocolade, room en boter.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    5, 'Caramel', false , 'mengsel van suiker, glucose, room en boter'
+    5, 'Caramel', false , 'Mengsel van suiker, room en boter.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    6, 'Nougatine', false , 'mengsel van amandelschilfers en suiker'
+    6, 'Nougatine', false , 'Mengsel van amandelschilfers en suiker.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    7, 'Pralinosa', false , 'Zwitserse praline'
+    7, 'Pralinosa', false , 'Zwitserse hazelnootpraline.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    8, 'Advocaat', true , 'mengsel van eieren, melk, suiker en alcohol'
+    8, 'Advocaat', true , 'Mengsel van eieren, melk, suiker en alcohol.'
     );
 
 insert into Vulling (
     ID, VULLING_NAME, ALCOHOL, INFO
 )VALUES (
-    9, 'confituur', false , 'mengsel van suiker en fruit'
+    9, 'Confituur', false , 'Mengsel van suiker en fruit.'
     );
 
 insert into PRALINE_VULLING_COLLECTION (
